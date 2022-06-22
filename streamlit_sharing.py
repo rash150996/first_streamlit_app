72d35f3ca08e0f486d8da6d0acdc6b6f83f447ce
Rashmi Ghosh <rashmighoshofficial963@gmail.com>
2022-06-22 22:21:04 +0530

    ADDED: the the basic script to my first app

---
+++ streamlit_sharing.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+from gsheetsdb import connect
+
+st.title("My first Streamlit WebApp")
+
+
+df = pd.DataFrame({"one":[1,2,3], "two":[4,5,6], "three":[7,8,9]})
+st.write(df)
+
+
+st.title("Connect to Google sheets")
+gsheet_url = st.secrets["public_gsheet_url"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
+
+#print(f'There are total of {bags * apples_in_bag} apples')
+#"https://docs.google.com/spreadsheets/d/1tXtSdJUrDFdQ2w4tvwAoE1ZIhIecNKYMCp1z7xwLvQY/edit?usp=sharing"
